11d4444f92be96f4bc147a8bcd74d4e82fbd53ef
Viktor <vik.tan@hotmail.com>
2023-01-07 11:38:15 -0600

    added "Results" page

--- src/Results/ResultsList.js
+++ src/Results/ResultsList.js
@@ -6,13 +6,13 @@ const ResultsList = (props) => {
     const {orders} = props;
 
     const resultConfig = [
-        {key: 'job', label: 'Job', render: (row) => <b>{row.job}</b>},
-        {key: 'employee', label: 'Employee', render: (row) => row.employee},
-        {key: 'net', label: 'Net Profit', render: (row) => `$${row.price - row.primeCost}`},
-        {key: 'income', label: 'Income', render: (row) => `$${row.price}`},
-        {key: 'prime', label: 'Prime Cost', render: (row) => `$${row.primeCost}`},
-        {key: 'paid', label: 'Paid Sum', render: (row) => `$${row.payments}`},
-        {key: 'debt', label: 'Client Debt', render: (row) => `$${row.price - row.payments}`},
+        {key: 'job', label: 'Job', render: (row) => <b>{row.service.job}</b>},
+        {key: 'employee', label: 'Employee', render: (row) => row.service.employee},
+        {key: 'net', label: 'Net Profit', render: (row) => `$${row.service.price - row.service.primeCost}`},
+        {key: 'income', label: 'Income', render: (row) => `$${row.service.price}`},
+        {key: 'prime', label: 'Prime Cost', render: (row) => `$${row.service.primeCost}`},
+        {key: 'paid', label: 'Paid Sum', render: (row) => `$${row.paid.payment}`},
+        {key: 'debt', label: 'Client Debt', render: (row) => `$${row.paid.debt}`},
     ]
 
     return (
@@ -43,19 +43,19 @@ const ResultsList = (props) => {
 
                     </td>
                     <td>
-                        ${orders.reduce((acc, el) => el.price - el.primeCost + acc, 0)}
+                        ${orders.reduce((acc, el) => el.service.price - el.service.primeCost + acc, 0)}
                     </td>
                     <td>
-                        ${orders.reduce((acc, el) => el.price + acc, 0)}
+                        ${orders.reduce((acc, el) => el.service.price + acc, 0)}
                     </td>
                     <td>
-                        ${orders.reduce((acc, el) => el.primeCost + acc, 0)}
+                        ${orders.reduce((acc, el) => el.service.primeCost + acc, 0)}
                     </td>
                     <td>
-                        ${orders.reduce((acc, el) => el.payments + acc, 0)}
+                        ${orders.reduce((acc, el) => el.paid.payment + acc, 0)}
                     </td>
                     <td>
-                        ${orders.reduce((acc, el) => el.price + acc, 0) - orders.reduce((acc, el) => el.payments + acc, 0)}
+                        ${orders.reduce((acc, el) => el.service.price + acc, 0) - orders.reduce((acc, el) => el.paid.payment + acc, 0)}
                     </td>
                 </tr>
                 </tfoot>
